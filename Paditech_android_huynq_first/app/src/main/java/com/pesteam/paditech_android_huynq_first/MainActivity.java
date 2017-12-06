04efe848c542ec7bbdd7b0832583a28b4e5594cb
Nguyễn Quang Huy <huynq1@appota.com>
2017-12-06 13:54:50 +0700

    create project

---
+++ Paditech_android_huynq_first/app/src/main/java/com/pesteam/paditech_android_huynq_first/MainActivity.java
@@ -0,0 +1,13 @@
+package com.pesteam.paditech_android_huynq_first;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
