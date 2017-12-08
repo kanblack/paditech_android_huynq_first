6eb4b6e687d76c76e771d305855b1d21deefff79
Nguyễn Quang Huy <huynq1@appota.com>
2017-12-08 11:29:20 +0700

    create screen2

---
+++ Paditech_android_huynq_first/app/src/main/java/com/pesteam/paditech_android_huynq_first/Screen2Activity.java
@@ -0,0 +1,13 @@
+package com.pesteam.paditech_android_huynq_first;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Screen2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen2);
+    }
+}
