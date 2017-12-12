9fe95361b52a9de99a67d69ca5875316f76182da
Nguyễn Quang Huy <huynq1@appota.com>
2017-12-12 18:40:07 +0700

    add get retrofit in screen1

--- Paditech_android_huynq_first/app/src/main/java/com/pesteam/paditech_android_huynq_first/Screen1Activity.java
+++ Paditech_android_huynq_first/app/src/main/java/com/pesteam/paditech_android_huynq_first/Screen1Activity.java
@@ -1,24 +1,62 @@
 package com.pesteam.paditech_android_huynq_first;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import com.pesteam.paditech_android_huynq_first.retrofit.APIService;
+import com.pesteam.paditech_android_huynq_first.retrofit.APIUtils;
+import com.pesteam.paditech_android_huynq_first.retrofit.GetDataScreen1.DataGet;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class Screen1Activity extends AppCompatActivity {
 
+    APIService mApiService = APIUtils.getAPIService();
     @BindView(R.id.rcvMain)
     RecyclerView rcvMain;
+    Screen1Adapter screen1Adapter = new Screen1Adapter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen_1);
         ButterKnife.bind(this);
-        rcvMain.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
-        rcvMain.setAdapter(new Screen1Adapter());
+        getData();
+    }
+
+    private void getData() {
+        Intent intent = getIntent();
+        String token = intent.getStringExtra("access_token");
+        getDataScreen1(token);
+    }
+
+    private void getDataScreen1(String access_token){
+        mApiService.loadData(access_token).enqueue(new Callback<DataGet>() {
+            @Override
+            public void onResponse(Call<DataGet> call, Response<DataGet> response) {
+                if(response.isSuccessful()){
+                    screen1Adapter.setGet(response.body().getData());
+                    Log.e( "onResponse: ", response.body().getData().getSection().size()+"");
+                    rcvMain.setLayoutManager(new LinearLayoutManager(getBaseContext(),LinearLayoutManager.VERTICAL,false));
+                    rcvMain.setAdapter(screen1Adapter);
+                }
+                else {
+                    Log.e( "onResponse: ", "unsucess");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<DataGet> call, Throwable t) {
+                Log.e("onreSponse", t.toString());
+            }
+        });
     }
 }
--- Paditech_android_huynq_first/app/src/main/java/com/pesteam/paditech_android_huynq_first/Screen1Adapter.java
+++ Paditech_android_huynq_first/app/src/main/java/com/pesteam/paditech_android_huynq_first/Screen1Adapter.java
@@ -2,14 +2,22 @@ package com.pesteam.paditech_android_huynq_first;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.pesteam.paditech_android_huynq_first.retrofit.GetDataScreen1.DataGet;
+import com.pesteam.paditech_android_huynq_first.retrofit.GetDataScreen1.Get;
+import com.squareup.picasso.Picasso;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by bangindong on 12/7/2017.
@@ -25,6 +33,7 @@ public class Screen1Adapter extends RecyclerView.Adapter<Screen1Adapter.BaseView
     private static final int TYPE_CHILD_2 = 6;
     private static final int TYPE_CHILD_3 = 7;
     private static final int TYPE_CHILD_4 = 8;
+    private Get get = new Get();
 
     @Override
     public BaseViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -46,7 +55,7 @@ public class Screen1Adapter extends RecyclerView.Adapter<Screen1Adapter.BaseView
                 holder = new ResearchViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.view_child3_rcv_screen1, parent, false));
                 break;
             case TYPE_CHILD_4:
-                holder = new SupportViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.view_child3_rcv_screen1, parent, false));
+                holder = new ResearchViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.view_child3_rcv_screen1, parent, false));
                 break;
             default:
                 holder = new ResearchViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.view_child3_rcv_screen1, parent, false));
@@ -70,7 +79,7 @@ public class Screen1Adapter extends RecyclerView.Adapter<Screen1Adapter.BaseView
 
     @Override
     public int getItemCount() {
-        return 8;
+        return get.getSection().size()*2;
     }
 
     abstract class BaseViewHolder extends RecyclerView.ViewHolder {
@@ -90,6 +99,8 @@ public class Screen1Adapter extends RecyclerView.Adapter<Screen1Adapter.BaseView
 
         @BindView(R.id.ln_text)
         LinearLayout layout;
+        @BindView(R.id.tx_title_section_screen1)
+        TextView tv_title;
 
         SectionViewHolder(View itemView) {
             super(itemView);
@@ -98,6 +109,7 @@ public class Screen1Adapter extends RecyclerView.Adapter<Screen1Adapter.BaseView
         @SuppressLint("ResourceAsColor")
         @Override
         void onBindingData(int position) {
+            tv_title.setText(get.getSection().get(position/2).getTitle());
             Context context = itemView.getContext();
             switch (position / 2) {
                 case 0:
@@ -120,29 +132,87 @@ public class Screen1Adapter extends RecyclerView.Adapter<Screen1Adapter.BaseView
 
     class NewsViewHolder extends BaseViewHolder {
 
+        @BindView(R.id.im_big_section1_screen1)
+        ImageView im_big;
+        @BindView(R.id.im_small_top_section1_screen1)
+                ImageView im_small_top;
+        @BindView(R.id.im_small_bottom_section1_screen1)
+                ImageView im_small_bottom;
+        @BindView(R.id.textView)
+                TextView title_big;
+        @BindView(R.id.textView2)
+                TextView short_descrip_big;
+        @BindView(R.id.textView3)
+                TextView title_small_top;
+        @BindView(R.id.textView4)
+                TextView title_small_bottom;
+
         NewsViewHolder(View itemView) {
             super(itemView);
         }
 
         @Override
         void onBindingData(int position) {
-
+            Picasso.with(itemView.getContext()).load(get.getSection().get((position-1)/2).getImages().get(0).getUrl()).into(im_big);
+            Picasso.with(itemView.getContext()).load(get.getSection().get((position-1)/2).getImages().get(1).getUrl()).into(im_small_top);
+            Picasso.with(itemView.getContext()).load(get.getSection().get((position-1)/2).getImages().get(2).getUrl()).into(im_small_bottom);
+            title_big.setText(get.getSection().get((position-1)/2).getImages().get(0).getTitle());
+            short_descrip_big.setText(get.getSection().get((position-1)/2).getImages().get(0).getShortDescription());
+            title_small_top.setText(get.getSection().get((position-1)/2).getImages().get(1).getTitle());
+            title_small_bottom.setText(get.getSection().get((position-1)/2).getImages().get(2).getTitle());
         }
     }
 
     class RequestInsuranceViewHolder extends BaseViewHolder {
 
-        public RequestInsuranceViewHolder(View itemView) {
+        @BindView(R.id.img_top_left_section2)
+                ImageView img_top_left;
+        @BindView(R.id.img_top_right_section2)
+        ImageView img_top_right;
+        @BindView(R.id.img_bottom_left_section2)
+        ImageView img_bottom_left;
+        @BindView(R.id.img_bottom_right_section2)
+        ImageView img_bottom_right;
+        @BindView(R.id.tex_top_left_section2)
+        TextView tex_top_left;
+        @BindView(R.id.tex_top_right_section2)
+        TextView tex_top_right;
+        @BindView(R.id.tex_bottom_left_section2)
+        TextView tex_bottom_left;
+        @BindView(R.id.tex_bottom_right_section2)
+        TextView tex_bottom_right;
+
+        RequestInsuranceViewHolder(View itemView) {
             super(itemView);
         }
 
         @Override
         void onBindingData(int position) {
-
+            Picasso.with(itemView.getContext()).load(get.getSection().get((position-1)/2).getImages().get(0).getUrl()).into(img_top_left);
+            Picasso.with(itemView.getContext()).load(get.getSection().get((position-1)/2).getImages().get(1).getUrl()).into(img_top_right);
+            Picasso.with(itemView.getContext()).load(get.getSection().get((position-1)/2).getImages().get(2).getUrl()).into(img_bottom_left);
+            Picasso.with(itemView.getContext()).load(get.getSection().get((position-1)/2).getImages().get(3).getUrl()).into(img_bottom_right);
+            tex_top_left.setText(get.getSection().get((position-1)/2).getImages().get(0).getTitle());
+            tex_top_right.setText(get.getSection().get((position-1)/2).getImages().get(1).getTitle());
+            tex_bottom_left.setText(get.getSection().get((position-1)/2).getImages().get(2).getTitle());
+            tex_bottom_right.setText(get.getSection().get((position-1)/2).getImages().get(3).getTitle());
         }
     }
 
     class ResearchViewHolder extends BaseViewHolder {
+
+        @BindView(R.id.cic_left_section3_screen1)
+        CircleImageView cic_left;
+        @BindView(R.id.cic_center_section3_screen1)
+        CircleImageView cic_center;
+        @BindView(R.id.cic_right_section3_screen1)
+        CircleImageView cic_right;
+        @BindView(R.id.tex_left_section3_screen1)
+        TextView tex_left;
+        @BindView(R.id.tex_center_section3_screen1)
+        TextView tex_center;
+        @BindView(R.id.tex_right_section3_screen1)
+        TextView tex_right;
 
         ResearchViewHolder(View itemView) {
             super(itemView);
@@ -150,7 +220,12 @@ public class Screen1Adapter extends RecyclerView.Adapter<Screen1Adapter.BaseView
 
         @Override
         void onBindingData(int position) {
-
+            Picasso.with(itemView.getContext()).load(get.getSection().get((position-1)/2).getImages().get(0).getUrl()).into(cic_left);
+            Picasso.with(itemView.getContext()).load(get.getSection().get((position-1)/2).getImages().get(1).getUrl()).into(cic_center);
+            Picasso.with(itemView.getContext()).load(get.getSection().get((position-1)/2).getImages().get(2).getUrl()).into(cic_right);
+            tex_left.setText(get.getSection().get((position-1)/2).getImages().get(0).getTitle());
+            tex_center.setText(get.getSection().get((position-1)/2).getImages().get(1).getTitle());
+            tex_right.setText(get.getSection().get((position-1)/2).getImages().get(2).getTitle());
         }
     }
 
@@ -164,5 +239,13 @@ public class Screen1Adapter extends RecyclerView.Adapter<Screen1Adapter.BaseView
         void onBindingData(int position) {
 
         }
+    }
+
+    public Get getGet() {
+        return get;
+    }
+
+    public void setGet(Get get) {
+        this.get = get;
     }
 }
